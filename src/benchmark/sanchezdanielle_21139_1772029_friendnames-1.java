7f80a03c57580c51a176f026573c48fbb9af27b4
Essa Imhmed <imhmede@Essas-MBP.home>
2025-03-12 20:01:00 -0600

    Commit sanchezdanielle_21139_1772029_friendnames-1.java

---
+++ src/benchmark/sanchezdanielle_21139_1772029_friendnames-1.java
@@ -0,0 +1,15 @@
+/**
+ * This program prints out 3 friend names on 3 separate lines
+ * @author Danielle Sanchez
+ * 09/20/23
+ */
+
+public class friendnames {
+    public static void main(String[] args)
+    {
+        /*This part prints the 3 names on 3 separate lines */
+        System.out.println("Patricia");
+        System.out.println("Courtney");
+        System.out.println("Ashley");
+    }
+}
